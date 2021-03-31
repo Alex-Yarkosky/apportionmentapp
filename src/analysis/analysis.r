2db5b4668be50069e952ee29a2c40e1b0d23f5aa
Alex Yarkosky <yarkoskya@allegheny.edu>
2021-03-31 17:13:18 -0400

    finalize graph code

--- src/analysis/analysis.r
+++ src/analysis/analysis.r
@@ -16,27 +16,28 @@ houseSizeApportionments <- read.csv(file.choose(), header = TRUE, sep = ",")
 censusYearApportionments <- read.csv(file.choose(), header = TRUE, sep = ",")
 
 # Filter data for variable house sizes dataset
-hamiltonHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "hamilton"), ]
-jeffersonHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "jefferson"), ]
-lowndesHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "lowndes"), ]
-adamsHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "adams"), ]
-websterHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "webster"), ]
-deanHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "dean"), ]
-huntingtonHillHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "huntington-hill"), ]
+hamiltonHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "Hamilton"), ]
+jeffersonHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "Jefferson"), ]
+lowndesHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "Lowndes"), ]
+adamsHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "Adams"), ]
+websterHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "Webster"), ]
+deanHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "Dean"), ]
+huntingtonHillHSA <- houseSizeApportionments[ which(houseSizeApportionments$method == "Huntington-Hill"), ]
 
 # Filter data for variable census years dataset
-hamiltonCYA <- censusYearApportionments[ which(censusYearApportionments$method == "hamilton"), ]
-jeffersonCYA <- censusYearApportionments[ which(censusYearApportionments$method == "jefferson"), ]
-lowndesCYA <- censusYearApportionments[ which(censusYearApportionments$method == "lowndes"), ]
-adamsCYA <- censusYearApportionments[ which(censusYearApportionments$method == "adams"), ]
-websterCYA <- censusYearApportionments[ which(censusYearApportionments$method == "webster"), ]
-deanCYA <- censusYearApportionments[ which(censusYearApportionments$method == "dean"), ]
-huntingtonHillCYA <- censusYearApportionments[ which(censusYearApportionments$method == "huntington-hill"), ]
+hamiltonCYA <- censusYearApportionments[ which(censusYearApportionments$method == "Hamilton"), ]
+jeffersonCYA <- censusYearApportionments[ which(censusYearApportionments$method == "Jefferson"), ]
+lowndesCYA <- censusYearApportionments[ which(censusYearApportionments$method == "Lowndes"), ]
+adamsCYA <- censusYearApportionments[ which(censusYearApportionments$method == "Adams"), ]
+websterCYA <- censusYearApportionments[ which(censusYearApportionments$method == "Webster"), ]
+deanCYA <- censusYearApportionments[ which(censusYearApportionments$method == "Dean"), ]
+huntingtonHillCYA <- censusYearApportionments[ which(censusYearApportionments$method == "Huntington-Hill"), ]
 
 # Plot house size lines together
 
-png(file = "tempHS.png")
-ggplot() + xlab("House Size") + ylab("District Population") + labs(color = "Method") +
+png(file = "houseSizeGraph.png")
+ggplot() + xlab("House Size") + ylab("District Population") + 
+  labs(color = "Method") + scale_y_continuous(labels = scales::label_comma()) +
   geom_line(data = hamiltonHSA, aes(x = house_size, y = ave_constituency, color = method)) + 
   geom_line(data = jeffersonHSA, aes(x = house_size, y = ave_constituency, color = method)) + 
   geom_line(data = lowndesHSA, aes(x = house_size, y = ave_constituency, color = method)) + 
@@ -48,8 +49,9 @@ dev.off()
 
 # Plot census year lines together
 
-png(file = "tempCY.png")
-ggplot() + xlab("Year") + ylab("District Population") + labs(color = "Method") +
+png(file = "censusYearGraph.png")
+ggplot() + xlab("Year") + ylab("District Population") + 
+  labs(color = "Method") + scale_y_continuous(labels = scales::label_comma()) +
   geom_line(data = hamiltonCYA, aes(x = year, y = ave_constituency, color = method)) + 
   geom_line(data = jeffersonCYA, aes(x = year, y = ave_constituency, color = method)) + 
   geom_line(data = lowndesCYA, aes(x = year, y = ave_constituency, color = method)) + 
